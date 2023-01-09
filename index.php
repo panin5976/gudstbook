304a9461f7dee09d97a8116c38cdbaed4ffbb4db
panin5976 <panin.j@ku.th>
2023-01-09 12:07:10 +0700

    falst commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>  
+    <from action"show.php">
+        what is your name<input type="text" name="name"><br>
+        what is your age<input type="text" name="age"><br>
+        <input type="submit" value"submit">
+    </from>
+    
+   
+
+</body>
+</html>
---
+++ show.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>  
+    <?php
+    $name = $_GET["name"]
+    $age = $_GET["age"]
+
+    echo "name" .$name."<be>"
+    echo "age".$age."<be>"
+    ?>
+</body>
+</html>
